cc0388d8f5dd0f66895b2507ad96e6a4f367ef24
Alberto Romeu <alrocar@cartodb.com>
2020-04-16 18:17:10 +0200

    test anonymous maps

--- carto/print.py
+++ carto/print.py
@@ -166,7 +166,8 @@ class Printer(object):
             }
 
     def prepare_url(self, tile_size, lon, lat, zoom):
-        return (self.server_url + '/api/v1/map/static/named/{template}/{tile_size}/{tile_size}.png?zoom={zoom}&lat={lat}&lon={lon}&api_key={api_key}').format(username=self.username, template=self.map_id, tile_size=tile_size, zoom=zoom, lat=lat, lon=lon, api_key=self.api_key)
+        # GET /api/v1/map/static/center/{token}/{z}/{lat}/{lng}/{width}/{height}.{format}{{?}extra_options}
+        return (self.server_url + '/api/v1/map/static/center/{template}/{zoom}/{lat}/{lon}/{tile_size}/{tile_size}.png?api_key={api_key}').format(username=self.username, template=self.map_id, tile_size=tile_size, zoom=zoom, lat=lat, lon=lon, api_key=self.api_key)
 
     def generate_filename(self):
         return '{username}_{map_id}_{date}'.format(username=self.username, map_id=self.sanitize(self.map_id), date=datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
